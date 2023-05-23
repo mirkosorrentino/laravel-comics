e9aaf782eb002ad4c235905a9134b0d4137b2653
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-05-23 16:01:35 +0200

    Project scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    <div class="text-center">Hello World!</div>
-</body>
-
-</html>
+@section('content')
+<div class="text-center">Ciao World!</div> 
+@endsection
